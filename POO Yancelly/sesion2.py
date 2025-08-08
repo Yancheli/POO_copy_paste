dd6d5915830523e3a4bfa64a932836ce3a3a0b24
yemc <yancellymolina12@gmail.com>
2025-08-08 12:13:54 -0500

    sesion2

---
+++ POO Yancelly/sesion2.py
@@ -0,0 +1,16 @@
+#Contador de vocales: Contar cuantas vocales tiene una cadena ingresada por el usuario, 
+# almacenando en un diccionario y pudiendo escoger después que tanto se repitió alguna vocal. 
+# El usuario ingresa la información y pregunta el número de veces que se repitió.
+
+# definicion
+vocal = "a e i o u"
+contar_vocales = {}
+texto = input("Ingrese su texto, por favor")
+texto = texto.lower() 
+
+
+
+
+# programa
+
+
