5ed0174f8b9c8b868d8bd47e2d50516ad8992f68
Miguel111206 <marboleda2006@gmail.com>
2025-08-08 12:19:43 -0500

    Ejercicio2

--- miguel/ejercicioedad.py
+++ miguel/ejercicioedad.py
@@ -5,4 +5,14 @@ Edad = int(Edad)
 if Edad < 18:
     print(f"Eres menor de edad. {Nombre}")
 else:
-    print(f"Eres mayor de edad. {Nombre}")
+   print(f"Eres mayor de edad. {Nombre}")
+    
+Persona = {
+        "Nombre": "Miguel" ,
+        "Edad": 18,
+        "Ciudad": "Medellín",
+        "Altura": 1.86,
+    } 
+print(Persona["Altura"])
+
+
