89b0f6c528672e975cd6731db8542097fe686878
CHERRYPOPUwU <erxiplay@gmail.com>
2025-10-23 23:13:57 -0500

    Subir base de datos a Render y cambiar config.py

--- trabajo final/ecom_login/config.py
+++ trabajo final/ecom_login/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = 'admin2201'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-123'
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'postgresql://postgres:admin2201@localhost:5432/ecom'
+        'postgresql://ecom_user_db:90NEKl6Us2GQHVs5zGri66fUIguWSI6t@dpg-d3tfbs2li9vc73bauok0-a.oregon-postgres.render.com/ecom_1w2u'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
